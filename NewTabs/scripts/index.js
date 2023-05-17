861f88c56c9666214951b63b33c0a1d7bfbd2b6b
acruz-23 <cruz.581@outlook.com>
2023-05-17 09:13:09 -0400

    Section 3-1 exercise.

---
+++ NewTabs/scripts/index.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName("imageGroup").style.border =
+  "2px solid #FF0000";
---
+++ querySelectorExercises/index.js
@@ -0,0 +1,8 @@
+// let objSet1 = document.querySelectorAll("p");
+// let objSet2 = document.querySelectorAll(".attn");
+// let objSet3 = document.querySelectorAll("p.attn");
+// let objSet4 = document.querySelectorAll("img[alt]");
+// let objSet5 = document.querySelectorAll("div > p");
+// let objSet6 = document.querySelectorAll("div, span");
+
+// document.getElementsByClassName("imageGroup").style.display = "none";
